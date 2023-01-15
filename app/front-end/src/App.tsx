95116dbed713decbbc733602753597f3851a55d2
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-01-14 23:26:13 -0300

    Implementing events in login page

--- app/front-end/src/App.tsx
+++ app/front-end/src/App.tsx
@@ -1,10 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
+import ClientsList from './components/AddClient';
+import CatPage from './pages/CatPage';
+import Home from './pages/HomePage';
 import Login from './pages/LoginPage';
+import RandomDog from './pages/RandomDogPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
+      <Route path="/home" element={ <Home /> } />
+      <Route path="/cat" element={ <CatPage /> } />
+      <Route path="/random-dog" element={ <RandomDog /> } />
+      <Route path="/clients-list" element={ <ClientsList /> } />
     </Routes>
   );
 }
--- app/front-end/src/pages/LoginPage.tsx
+++ app/front-end/src/pages/LoginPage.tsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [keepConneted, setKeepConnected] = useState(false);
+  const [failedLogin, setFailedLogin] = useState(false);
+
+  useEffect(() => {
+    setFailedLogin(false);
+  }, [username, password]);
+
+  let navigate = useNavigate();
+
+  const requestLogin = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+
+    try {
+      navigate('/home');
+    } catch (error) {
+      setFailedLogin(true);
+    }
+  };
+
   return (
     <main>
       <div>
@@ -6,18 +32,42 @@ export default function Login() {
           <h1>Welcome Back!</h1>
           <label htmlFor="username">
             Username
-            <input type="text" id="username" name="username" required />
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+              required
+            />
           </label>
           <label htmlFor="password">
             Password
-            <input type="text" id="password" name="password" required />
+            <input
+              type="text"
+              id="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+              required
+            />
           </label>
           <label htmlFor="keep-connected">
-            <input type="checkbox" id="keep-connected" name="keep-connected" />
+            <input
+              type="checkbox"
+              id="keep-connected"
+              checked={keepConneted}
+              onChange={() => setKeepConnected(!keepConneted)}
+            />
             remeber me
           </label>
-          <button type="submit">Login</button>
-         </form>
+          {failedLogin && (
+            <p>
+              *Sorry, your username or password is incorrect. Please, try again.
+            </p>
+          )}
+          <button type="submit" onClick={(event) => requestLogin(event)}>
+            Login
+          </button>
+        </form>
       </div>
     </main>
   );

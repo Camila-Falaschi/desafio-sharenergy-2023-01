a650342ec06eae79450547ca01a55171d8698346
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-01-17 17:49:45 -0300

    page HTTP Cat is not working properly because of the cors policy error

--- app/front-end/src/pages/CatPage.tsx
+++ app/front-end/src/pages/CatPage.tsx
@@ -1,19 +1,53 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import ImgCatDefault from "../images/default-cat.svg";
+import catNotFound from "../images/not-found-cat.svg";
+import catCodeImage from "../services/APIHTTPCats";
 
 export default function CatPage() {
-  // https://http.cat/
-  
+  const [imageSrc, setImageSrc] = useState(ImgCatDefault);
+
+  useEffect(() => {}, [imageSrc]);
+
+  const selectInputRange = (start: number, end: number) => {
+    return Array(end - start + 1)
+      .fill(null)
+      .map((_, index) => start + index);
+  };
+
+  const changingCatImage = async (codeNumber: string) => {
+    const image = await catCodeImage(codeNumber);
+    setImageSrc(image);
+  };
+
   return (
     <>
       <Header />
       <main>
         <div>
           <h1>HTTP Cat response status code</h1>
-          <select id='cat-status-code'>
+          <select
+            id="cat-status-code"
+            onChange={({ target }) => changingCatImage(target.value)}
+          >
             <option value="">--choose a status code--</option>
+            {selectInputRange(100, 599).map((codeNumber) => {
+              return (
+                <option key={codeNumber} value={codeNumber}>
+                  {codeNumber}
+                </option>
+              );
+            })}
           </select>
+        </div>
+        <div>
+          {imageSrc === undefined ? (
+            <img src={catNotFound} alt="cat not found" />
+          ) : (
+            <img src={imageSrc} alt={imageSrc.split("/")[2]} />
+          )}
         </div>
       </main>
     </>
-  )
+  );
 }
---
+++ app/front-end/src/services/APIHTTPCats.tsx
@@ -0,0 +1,19 @@
+// Error - CORS policy: Access to fetch at 'https://http.cat/100' from origin 'http://localhost:3000' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.
+
+export default async function catCodeImage(code: string) {
+  try {
+    const response = await fetch(`https://http.cat/${code}`, {
+      // mode: 'no-cors',
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Request-Method": "GET",
+        "Access-Control-Allow-Hearders": "*",
+      },
+    });
+
+    return response.json();
+  } catch (e) {
+    console.log(e);
+    ;
+  }
+}
